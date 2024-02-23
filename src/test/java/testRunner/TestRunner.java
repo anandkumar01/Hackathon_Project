7a024b27366aea2e26506140e256049d8e103ade
Anand <Anand.Vishwakarma@cognizant.com>
2024-02-23 09:44:44 +0530

    Json report implemented

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = { ".\\src\\test\\resources\\FeatureFiles" }, glue = { "stepDefinition" }, plugin = {
-		"pretty", "html:reports/myreport.html",
+		"pretty", "html:reports/myreport.html", "json:target/report.json",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, dryRun = false, monochrome = true, publish = true, tags = "@sanity")
 
 public class TestRunner extends AbstractTestNGCucumberTests {
