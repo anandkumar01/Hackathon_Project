abf1f041e687becc54ad90b4c2a3cec87104a367
Anand <Anand.Vishwakarma@cognizant.com>
2024-03-01 17:20:23 +0530

    Cucumber testNG implemented

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -1,15 +1,15 @@
 package testRunner;
 
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(features = { ".\\src\\test\\resources\\FeatureFiles" }, glue = { "stepDefinition" }, plugin = {
 		"pretty", "html:reports/myreport.html",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, dryRun = false, monochrome = true, publish = true, tags = "@sanity")
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
